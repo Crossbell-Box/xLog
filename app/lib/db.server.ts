69355707a78362192623fbbec73d70ea1b9b854c
EGOIST <hi@egoist.sh>
2022-05-07 20:27:35 +0800

    log query time

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,11 +1,18 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, type Prisma } from "@prisma/client"
 import { IS_PROD } from "./constants"
 import { isPrimaryRegion } from "./env"
 import { singleton } from "./singleton.server"
 
+const logLevel: Prisma.LogLevel[] = IS_PROD
+  ? ["info", "error", "warn"]
+  : ["info", "warn", "error", "query"]
+
 export const prismaWrite = /* @__PURE__ */ singleton(
   "prisma-write",
-  () => new PrismaClient()
+  () =>
+    new PrismaClient({
+      log: logLevel,
+    })
 )
 
 // Connect to the read replica of our database on Fly
@@ -15,12 +22,25 @@ export const prismaRead = /* @__PURE__ */ singleton("prisma-read", () => {
   if (!isPrimaryRegion() && IS_PROD) {
     url = url.replace(":5432", ":5433")
   }
-  console.log("read replica url", url)
-  return new PrismaClient({
+  console.log("read replica url", url.replace(/\w+@/, "PASSWORD@"))
+  const client = new PrismaClient({
     datasources: {
       db: {
         url,
       },
     },
+    log: logLevel,
   })
+
+  client.$use(async (params, next) => {
+    const now = performance.now()
+    const result = await next(params)
+    const end = performance.now()
+    const duration = Math.floor(end - now)
+    console.log("query took", duration, "ms")
+    // See results here
+    return result
+  })
+
+  return client
 })
