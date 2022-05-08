aa28d69dd6e426a0a73f663c2a9d587751788828
EGOIST <hi@egoist.sh>
2022-05-08 13:23:01 +0800

    fix: properly save image extension

--- app/lib/upload.server.ts
+++ app/lib/upload.server.ts
@@ -3,21 +3,26 @@ import {
   type UploadHandler,
 } from "@remix-run/node"
 import { nanoid } from "nanoid"
+import path from "path"
 import { s3uploadFile } from "./s3.server"
 
 export const uploadImage = async (
   request: Request,
   { userId }: { userId: string }
 ) => {
-  const uploadHandler: UploadHandler = async ({ stream, mimetype }) => {
+  const uploadHandler: UploadHandler = async ({
+    stream,
+    mimetype,
+    filename,
+  }) => {
     const chunks = []
     for await (let chunk of stream) {
       chunks.push(chunk)
     }
     const buffer = Buffer.concat(chunks)
-    const filename = `${userId}/${nanoid()}.jpg`
-    await s3uploadFile(filename, buffer, mimetype)
-    return filename
+    const saveAs = `${userId}/${nanoid()}${path.extname(filename) || ".jpg"}`
+    await s3uploadFile(saveAs, buffer, mimetype)
+    return saveAs
   }
 
   const formData = await unstable_parseMultipartFormData(request, uploadHandler)
