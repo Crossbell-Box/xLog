744a47f41680436671ded720d1b5c890c9fae31f
EGOIST <hi@egoist.sh>
2022-05-12 01:25:09 +0800

    redirect unauthenticated users to the home page

---
+++ src/pages/dashboard/_middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server"
+import { AUTH_COOKIE_NAME } from "~/lib/env.server"
+
+export default function middleware(req: NextRequest) {
+  // Redirect unauthenticated users to the home page
+  if (!req.cookies[AUTH_COOKIE_NAME]) {
+    const url = req.nextUrl.clone()
+    url.pathname = "/"
+    return NextResponse.redirect(url)
+  }
+  return NextResponse.next()
+}
