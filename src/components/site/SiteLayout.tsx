23953a12259a90498f653c11d6bb0f2cdc0a1a92
DIYgod <i@diygod.me>
2022-07-28 19:33:15 +0100

    fix: types errors

--- src/components/site/SiteLayout.tsx
+++ src/components/site/SiteLayout.tsx
@@ -50,7 +50,6 @@ export const SiteLayout: React.FC<SiteLayoutProps> = ({
         description={site?.bio}
         icon={site?.avatars?.[0]}
         subscribed={!!subscription}
-        viewer={viewer}
       />
       <style>{ site?.css }</style>
       <div className={clsx(`max-w-screen-md mx-auto px-5 pb-12`, `pt-12`)}>
