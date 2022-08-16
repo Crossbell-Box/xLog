612bf76b41a20fb66d3eb358242449c9b3b85b06
DIYgod <i@diygod.me>
2022-08-16 21:23:35 +0100

    fix: missing GITHUB_LINK

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,5 +7,6 @@ export const SITE_URL = `${IS_PROD ? "https" : "http"}://${OUR_DOMAIN}`
 export const R2_URL = process.env.NEXT_PUBLIC_R2_URL
 export const APP_DESCRIPTION = process.env.APP_DESCRIPTION
 export const DISCORD_LINK = process.env.NEXT_PUBLIC_DISCORD_LINK
+export const GITHUB_LINK = process.env.NEXT_PUBLIC_GITHUB_LINK
 export const CSB_IO = process.env.NEXT_PUBLIC_CSB_IO
 export const CSB_SCAN = process.env.NEXT_PUBLIC_CSB_SCAN
