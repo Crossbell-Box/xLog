f7f422018d470e02de45ebbf58bd7414d1250673
DIYgod <i@diygod.me>
2022-08-17 10:07:29 +0100

    feat: priority display for users with avatars

--- src/components/site/PostFooter.tsx
+++ src/components/site/PostFooter.tsx
@@ -179,16 +179,21 @@ export const PostFooter: React.FC<{
           className="-space-x-4 mr-10 cursor-pointer"
           onClick={() => setIsLikeListOpen(true)}
         >
-          {likes.data?.list?.slice(0, 3)?.map((like: any, index) => (
-            <li className="inline-block" key={index}>
-              <Avatar
-                className="align-middle border-2 border-white"
-                images={like.fromCharacter?.metadata?.content?.avatars || []}
-                name={like.fromCharacter?.metadata?.content?.name}
-                size={40}
-              />
-            </li>
-          ))}
+          {likes.data?.list
+            ?.sort((a, b) =>
+              b.fromCharacter?.metadata?.content?.avatars ? 1 : -1,
+            )
+            .slice(0, 3)
+            ?.map((like: any, index) => (
+              <li className="inline-block" key={index}>
+                <Avatar
+                  className="align-middle border-2 border-white"
+                  images={like.fromCharacter?.metadata?.content?.avatars || []}
+                  name={like.fromCharacter?.metadata?.content?.name}
+                  size={40}
+                />
+              </li>
+            ))}
           {(likes.data?.count || 0) > 3 && (
             <li className="inline-block">
               <div className="align-middle border-2 border-white w-10 h-10 rounded-full inline-flex bg-gray-100 items-center justify-center text-gray-400 font-medium">
@@ -213,16 +218,21 @@ export const PostFooter: React.FC<{
           className="-space-x-4 cursor-pointer"
           onClick={() => setIsMintListOpen(true)}
         >
-          {mints.data?.list?.slice(0, 3).map((mint: any, index) => (
-            <li className="inline-block" key={index}>
-              <Avatar
-                className="align-middle border-2 border-white"
-                images={mint.character?.metadata?.content?.avatars || []}
-                name={mint.character?.metadata?.content?.name}
-                size={40}
-              />
-            </li>
-          ))}
+          {mints.data?.list
+            ?.sort((a, b: any) =>
+              b.character?.metadata?.content?.avatars ? 1 : -1,
+            )
+            .slice(0, 3)
+            .map((mint: any, index) => (
+              <li className="inline-block" key={index}>
+                <Avatar
+                  className="align-middle border-2 border-white"
+                  images={mint.character?.metadata?.content?.avatars || []}
+                  name={mint.character?.metadata?.content?.name}
+                  size={40}
+                />
+              </li>
+            ))}
           {(mints.data?.count || 0) > 3 && (
             <li className="inline-block">
               <div className="align-middle border-2 border-white w-10 h-10 rounded-full inline-flex bg-gray-100 items-center justify-center text-gray-400 font-medium">
