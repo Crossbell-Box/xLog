b7dc9051aee86dee85f712fc5d3234f089e4433c
DIYgod <i@diygod.me>
2022-09-21 19:31:02 +0100

    feat: remove google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,18 +3,7 @@ import { Html, Head, Main, NextScript, DocumentProps } from "next/document"
 export default function Document(props: DocumentProps) {
   return (
     <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
