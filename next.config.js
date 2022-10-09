6ecf9fc7dc45f80be8f83c5e48ec183860bd01ad
DIYgod <i@diygod.me>
2022-10-09 01:34:54 +0100

    chore: use git commit hash as nextjs build id

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const { withIpfsGateway } = require("@crossbell/ipfs-gateway-next")
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
+const execSync = require("child_process").execSync
+
+const lastCommitCommand = "git rev-parse HEAD"
 
 class UnoCSS {
   /**
@@ -50,6 +53,10 @@ module.exports = withBundleAnalyzer(
       contentSecurityPolicy:
         "default-src 'self'; script-src 'none'; sandbox; style-src 'unsafe-inline';",
       remotePatterns: [{ hostname: "**" }],
+    },
+
+    async generateBuildId() {
+      return execSync(lastCommitCommand).toString().trim()
     },
   }),
 )
