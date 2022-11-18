cd42f65df8cd90e2e8581da0bd0192b11ae0e498
DIYgod <i@diygod.me>
2022-11-18 16:39:24 +0000

    fix: tags page query

--- src/pages/api/pages.ts
+++ src/pages/api/pages.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { getPagesBySite } from "~/models/page.model"
+import { PageVisibilityEnum } from "~/lib/types"
 
 export default async function handler(
   req: NextApiRequest,
@@ -7,7 +8,16 @@ export default async function handler(
 ) {
   const query = req.query
 
-  const result = await getPagesBySite(query as any)
+  const result = await getPagesBySite({
+    site: query.site as string,
+    type: query.type as "post" | "page",
+    visibility: query.visibility as PageVisibilityEnum,
+    take: query.take ? parseInt(query.take as string) : null,
+    cursor: query.cursor as string,
+    tags: Array.isArray(query.tags)
+      ? (query.tags as string[])
+      : [query.tags as string],
+  })
 
   res.status(200).json(result)
 }
