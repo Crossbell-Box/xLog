5b844d6a148192e195bac0cc93f7ddd1e5bf654d
Runjuu <hi@runjuu.com>
2023-02-11 14:54:40 -0800

    fix: initial connect wallet issues

--- src/lib/wallet-config.ts
+++ src/lib/wallet-config.ts
@@ -1,46 +1,21 @@
 import { configureChains } from "wagmi"
+import { crossbell } from "wagmi/chains"
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc"
 import { MetaMaskConnector } from "wagmi/connectors/metaMask"
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect"
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet"
 import { InjectedConnector } from "wagmi/connectors/injected"
 
-import { CSB_SCAN, IPFS_GATEWAY, APP_NAME } from "~/lib/env"
+import { APP_NAME } from "~/lib/env"
 
 export const { chains, provider } = configureChains(
-  [
-    {
-      id: 3737,
-      name: "Crossbell",
-      network: "crossbell",
-      rpcUrls: {
-        default: {
-          http: ["https://rpc.crossbell.io"],
-        },
-      },
-      iconUrl: `${IPFS_GATEWAY}QmS8zEetTb6pwdNpVjv5bz55BXiSMGP9BjTJmNcjcUT91t`,
-      nativeCurrency: {
-        decimals: 18,
-        name: "Crossbell Token",
-        symbol: "CSB",
-      },
-      blockExplorers: {
-        default: {
-          name: "Crossbell Explorer",
-          url: CSB_SCAN,
-        },
-      },
-      testnet: false,
-    } as any,
-  ],
+  [crossbell],
   [
     jsonRpcProvider({
-      rpc: (chain) => ({ http: chain.rpcUrls.default.http[0] }),
+      rpc: () => ({ http: crossbell.rpcUrls.default.http[0] }),
     }),
   ],
-  {
-    pollingInterval: 1000,
-  },
+  { pollingInterval: 1000 },
 )
 
 export const connectors = [
