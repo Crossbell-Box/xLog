8324beaa096cd0bf0dcb6a7a6e3c26bce090d3bb
DIYgod <i@diygod.me>
2023-03-06 01:41:18 +0000

    feat: restrict the allowed characters for slug

--- src/pages/dashboard/[subdomain]/editor.tsx
+++ src/pages/dashboard/[subdomain]/editor.tsx
@@ -140,6 +140,14 @@ export default function SubdomainEditor() {
           ),
         )
       }
+      if (key === "slug" && !/^[a-zA-Z0-9\-_]*$/.test(value as string)) {
+        toast.error(
+          t(
+            "Slug can only contain letters, numbers, hyphens, and underscores.",
+          ),
+        )
+        return
+      }
       const newValues = { ...values, [key]: value }
       if (draftKey) {
         setStorage(draftKey, {
