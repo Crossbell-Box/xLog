3ad1abf9752981c2afb0d293632dba5023e24c25
DIYgod <i@diygod.me>
2023-03-22 01:44:37 +0000

    feat: support zh-TW

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "zh", "ja"],
+    locales: ["en", "zh", "zh-TW", "ja"],
     reloadOnPrerender: process.env.NODE_ENV === "development",
   },
 }
--- src/lib/language-detector.ts
+++ src/lib/language-detector.ts
@@ -3,9 +3,18 @@ import url from "url"
 
 export const languageDetector = (ctx: GetServerSidePropsContext) => {
   const queryLang = url.parse(ctx.req.url!, true).query.lang as string
-  return (
-    queryLang ||
-    ctx.req.headers["accept-language"]?.split(",")[0].split("-")[0] ||
-    "en"
-  )
+
+  let acceptLang = ctx.req.headers["accept-language"]?.split(",")[0]
+
+  if (acceptLang === "zh-CN") {
+    acceptLang = "zh"
+  } else if (acceptLang === "zh-HK") {
+    acceptLang = "zh-TW"
+  } else if (acceptLang === "zh-TW") {
+    // do nothing
+  } else {
+    acceptLang = acceptLang?.split("-")[0]
+  }
+
+  return queryLang || acceptLang || "en"
 }
