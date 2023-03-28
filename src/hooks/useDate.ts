c0eace8efef5c2f3c07ea506a7f54d44c074cb70
DIYgod <i@diygod.me>
2023-03-29 00:27:28 +0100

    feat: dayjs locale zh-tw

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -8,6 +8,7 @@ import relativeTime from "dayjs/plugin/relativeTime"
 import { useTranslation } from "next-i18next"
 import "dayjs/locale/en"
 import "dayjs/locale/zh"
+import "dayjs/locale/zh-tw"
 import "dayjs/locale/ja"
 
 dayjs.extend(localizedFormat)
