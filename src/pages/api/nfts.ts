395b86be062602c2d8320989591286e5567accd5
DIYgod <i@diygod.me>
2023-04-05 02:14:07 +0100

    fix: try to fix nft api

--- src/pages/api/nfts.ts
+++ src/pages/api/nfts.ts
@@ -25,35 +25,40 @@ export default async function handler(
   if (cache) {
     res.status(200).json(JSON.parse(cache))
   } else {
-    // const result = await getNFTs(query.address as string)
-    // await Promise.all(
-    //   result.list.map(async (nft: Asset) => {
-    //     if (!nft.items?.[0].mime_type && nft.items?.[0]?.address) {
-    //       try {
-    //         const mime_type = await cacheGet({
-    //           key: `nft-mimetype/${nft.items[0].address}`,
-    //           getValueFun: async () => {
-    //             const head = await fetch(
-    //               `${nft.items![0].address!.replace(
-    //                 IPFS_GATEWAY,
-    //                 "https://gateway.ipfs.io/ipfs/",
-    //               )}`,
-    //               {
-    //                 method: "HEAD",
-    //               },
-    //             )
-    //             return head.headers.get("content-type")
-    //           },
-    //         })
-    //         nft.items[0].mime_type = mime_type
-    //       } catch (error) {
-    //         console.warn(error)
-    //       }
-    //     }
-    //     return nft
-    //   }),
-    // )
-    // redis.set(redisKey, JSON.stringify(result), "EX", 60 * 60 * 24)
+    const result = await getNFTs(query.address as string)
+    await Promise.all(
+      result.list.map(async (nft: Asset) => {
+        if (!nft.items?.[0].mime_type && nft.items?.[0]?.address) {
+          try {
+            new URL(nft.items[0].address)
+          } catch (error) {
+            return nft
+          }
+          try {
+            const mime_type = await cacheGet({
+              key: `nft-mimetype/${nft.items[0].address}`,
+              getValueFun: async () => {
+                const head = await fetch(
+                  `${nft.items![0].address!.replace(
+                    IPFS_GATEWAY,
+                    "https://gateway.ipfs.io/ipfs/",
+                  )}`,
+                  {
+                    method: "HEAD",
+                  },
+                )
+                return head.headers.get("content-type")
+              },
+            })
+            nft.items[0].mime_type = mime_type
+          } catch (error) {
+            console.warn(error)
+          }
+        }
+        return nft
+      }),
+    )
+    redis.set(redisKey, JSON.stringify(result), "EX", 60 * 60 * 24)
     res.status(200).json({
       list: [],
     })
