734e6d282fa359b1f69e7147a728876c54e5e408
daidr <daidr@daidr.me>
2023-04-15 02:42:29 +0800

    fix: wrong popper position

--- src/components/ui/EditorToolbar.tsx
+++ src/components/ui/EditorToolbar.tsx
@@ -1,6 +1,6 @@
 import { EditorView } from "@codemirror/view"
 import { cn } from "~/lib/utils"
-import { FC, useRef } from "react"
+import { FC, useState } from "react"
 import { ICommand } from "~/editor"
 import { Tooltip } from "./Tooltip"
 import { useTranslation } from "next-i18next"
@@ -17,56 +17,82 @@ enum ToolbarMode {
   Preview,
 }
 
+type IToolbarItemProps = ICommand<any> & { view?: EditorView }
+
+const ToolbarItemWithPopover: FC<IToolbarItemProps> = ({
+  name,
+  icon,
+  label,
+  execute,
+  ui,
+  view,
+}) => {
+  const { t } = useTranslation("dashboard")
+  let [popoverButtonRef, setPopoverButtonRef] =
+    useState<HTMLButtonElement | null>(null)
+  let [popoverPanelRef, setPopoverPanelRef] = useState<HTMLDivElement | null>(
+    null,
+  )
+  let { styles: popStyles, attributes: popAttributes } = usePopper(
+    popoverButtonRef,
+    popoverPanelRef,
+  )
+  return (
+    <Popover key={name}>
+      {({ open }: { open: boolean }) => (
+        <>
+          <Tooltip key={name} label={t(label)} placement="bottom">
+            <Popover.Button
+              key={name}
+              title={name}
+              className="w-9 h-9 transition-colors text-lg border border-transparent rounded flex items-center justify-center text-zinc-500 group-hover:text-zinc-600 hover:text-zinc-500 hover:border-zinc-300 hover:bg-zinc-100"
+              ref={setPopoverButtonRef}
+            >
+              <span className={icon}></span>
+            </Popover.Button>
+          </Tooltip>
+          <Popover.Panel
+            ref={setPopoverPanelRef}
+            className="z-10"
+            style={popStyles.popper}
+            {...popAttributes.popper}
+          >
+            {ui &&
+              view &&
+              ui({
+                transferPayload: (payload) => {
+                  view &&
+                    execute({
+                      view,
+                      options: { container: view.dom },
+                      payload,
+                    })
+                },
+                view,
+              })}
+          </Popover.Panel>
+        </>
+      )}
+    </Popover>
+  )
+}
+
 export const EditorToolbar: FC<EditorToolbarProps> = ({ view, toolbars }) => {
   const { t } = useTranslation("dashboard")
-  const popoverButtonRef = useRef(null)
-  const popoverPanelRef = useRef(null)
-  let { styles: popStyles, attributes: popAttributes } = usePopper(
-    popoverButtonRef.current,
-    popoverPanelRef.current,
-  )
-
   const renderToolbar =
     (mode: ToolbarMode) =>
     // eslint-disable-next-line react/display-name
     ({ name, icon, label, execute, ui }: ICommand) => {
       return ui ? (
-        <Popover key={name}>
-          {({ open }: { open: boolean }) => (
-            <>
-              <Tooltip key={name} label={t(label)} placement="bottom">
-                <Popover.Button
-                  key={name}
-                  title={name}
-                  className="w-9 h-9 transition-colors text-lg border border-transparent rounded flex items-center justify-center text-zinc-500 group-hover:text-zinc-600 hover:text-zinc-500 hover:border-zinc-300 hover:bg-zinc-100"
-                  ref={popoverButtonRef}
-                >
-                  <span className={icon}></span>
-                </Popover.Button>
-              </Tooltip>
-              <Popover.Panel
-                ref={popoverPanelRef}
-                className="z-10"
-                style={popStyles.popper}
-                {...popAttributes.popper}
-              >
-                {ui &&
-                  view &&
-                  ui({
-                    transferPayload: (payload) => {
-                      view &&
-                        execute({
-                          view,
-                          options: { container: view.dom },
-                          payload,
-                        })
-                    },
-                    view,
-                  })}
-              </Popover.Panel>
-            </>
-          )}
-        </Popover>
+        <ToolbarItemWithPopover
+          key={name}
+          name={name}
+          icon={icon}
+          label={label}
+          execute={execute}
+          ui={ui}
+          view={view}
+        />
       ) : (
         <Tooltip key={name} label={t(label)} placement="bottom">
           <button
