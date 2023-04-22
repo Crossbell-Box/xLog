639897092cfe7e97b577336a67d243971d36196a
daidr <daidr@daidr.me>
2023-04-22 23:52:51 +0800

    fix: APlayer memo

--- src/components/ui/APlayer.tsx
+++ src/components/ui/APlayer.tsx
@@ -10,43 +10,38 @@ export const APlayer: React.FC<
     cover?: string
     lrc?: string
   } & React.AudioHTMLAttributes<HTMLAudioElement>
-> = memo(
-  function APlayer({ src, name, artist, cover, lrc, muted, autoPlay, loop }) {
-    if (!src) return null
+> = memo(function APlayer({
+  src,
+  name,
+  artist,
+  cover,
+  lrc,
+  muted,
+  autoPlay,
+  loop,
+}) {
+  if (!src) return null
 
-    src = toGateway(src)
-    if (cover) {
-      cover = toGateway(cover)
-    }
-    if (name) {
-      name = name.replace(/^user-content-/, "")
-    }
+  src = toGateway(src)
+  if (cover) {
+    cover = toGateway(cover)
+  }
+  if (name) {
+    name = name.replace(/^user-content-/, "")
+  }
 
-    return (
-      <AplayerReact
-        audio={{
-          name: name || "xLog audio",
-          artist: artist || "",
-          cover: cover || "/assets/logo.png",
-          lrc,
-          url: src,
-        }}
-        volume={muted ? 0 : 1}
-        autoPlay={autoPlay}
-        initialLoop={loop ? "one" : "none"}
-      />
-    )
-  },
-  (prevProps, nextProps) => {
-    return (
-      prevProps.src === nextProps.src &&
-      prevProps.name === nextProps.name &&
-      prevProps.artist === nextProps.artist &&
-      prevProps.cover === nextProps.cover &&
-      prevProps.lrc === nextProps.lrc &&
-      prevProps.muted === nextProps.muted &&
-      prevProps.autoPlay === nextProps.autoPlay &&
-      prevProps.loop === nextProps.loop
-    )
-  },
-)
+  return (
+    <AplayerReact
+      audio={{
+        name: name || "xLog audio",
+        artist: artist || "",
+        cover: cover || "/assets/logo.png",
+        lrc,
+        url: src,
+      }}
+      volume={muted ? 0 : 1}
+      autoPlay={autoPlay}
+      initialLoop={loop ? "one" : "none"}
+    />
+  )
+})
