47f3f6d08d1f3cb34b98752ec27689e54b9272d8
daidr <daidr@daidr.me>
2023-04-22 22:22:03 +0800

    feat: APlayer memo

--- src/components/ui/APlayer.tsx
+++ src/components/ui/APlayer.tsx
@@ -1,4 +1,5 @@
 import { APlayer as AplayerReact } from "aplayer-react"
+import { memo } from "react"
 
 import { toGateway } from "~/lib/ipfs-parser"
 
@@ -9,29 +10,43 @@ export const APlayer: React.FC<
     cover?: string
     lrc?: string
   } & React.AudioHTMLAttributes<HTMLAudioElement>
-> = ({ src, name, artist, cover, lrc, muted, autoPlay, loop }) => {
-  if (!src) return null
+> = memo(
+  function APlayer({ src, name, artist, cover, lrc, muted, autoPlay, loop }) {
+    if (!src) return null
 
-  src = toGateway(src)
-  if (cover) {
-    cover = toGateway(cover)
-  }
-  if (name) {
-    name = name.replace(/^user-content-/, "")
-  }
+    src = toGateway(src)
+    if (cover) {
+      cover = toGateway(cover)
+    }
+    if (name) {
+      name = name.replace(/^user-content-/, "")
+    }
 
-  return (
-    <AplayerReact
-      audio={{
-        name: name || "xLog audio",
-        artist: artist || "",
-        cover: cover || "/assets/logo.png",
-        lrc,
-        url: src,
-      }}
-      volume={muted ? 0 : 1}
-      autoPlay={autoPlay}
-      initialLoop={loop ? "one" : "none"}
-    />
-  )
-}
+    return (
+      <AplayerReact
+        audio={{
+          name: name || "xLog audio",
+          artist: artist || "",
+          cover: cover || "/assets/logo.png",
+          lrc,
+          url: src,
+        }}
+        volume={muted ? 0 : 1}
+        autoPlay={autoPlay}
+        initialLoop={loop ? "one" : "none"}
+      />
+    )
+  },
+  (prevProps, nextProps) => {
+    return (
+      prevProps.src === nextProps.src &&
+      prevProps.name === nextProps.name &&
+      prevProps.artist === nextProps.artist &&
+      prevProps.cover === nextProps.cover &&
+      prevProps.lrc === nextProps.lrc &&
+      prevProps.muted === nextProps.muted &&
+      prevProps.autoPlay === nextProps.autoPlay &&
+      prevProps.loop === nextProps.loop
+    )
+  },
+)
