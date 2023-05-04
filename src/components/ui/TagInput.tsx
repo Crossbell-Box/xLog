8c2e676ac4553c6fa7555aea6f497de9a4160fe3
birdgg <birdeggegg@gmail.com>
2023-05-04 20:49:44 +0800

    fix: style

--- src/components/ui/TagInput.tsx
+++ src/components/ui/TagInput.tsx
@@ -3,13 +3,10 @@ import { toast } from "react-hot-toast"
 import { shallow } from "zustand/shallow"
 
 import { Combobox, Transition } from "@headlessui/react"
-import {
-  CheckIcon,
-  ChevronUpDownIcon,
-  XMarkIcon,
-} from "@heroicons/react/20/solid"
+import { ChevronUpDownIcon, XMarkIcon } from "@heroicons/react/20/solid"
 
 import { useEditorState } from "~/hooks/useEdtiorState"
+import { cn } from "~/lib/utils"
 
 interface Props extends HTMLInputElement {
   userTags: string[]
@@ -50,7 +47,7 @@ export function TagInput({ userTags = [], className, id, name }: Props) {
   const filteredTags =
     query === ""
       ? userTags
-      : userTags.filter((tag) =>
+      : Array.from(new Set([query, ...userTags])).filter((tag) =>
           tag
             .toLowerCase()
             .replace(/\s+/g, "")
@@ -62,11 +59,7 @@ export function TagInput({ userTags = [], className, id, name }: Props) {
       <div className="flex flex-wrap gap-2 mb-2">
         {editorTags.map((tag) => (
           <div
-            className="text-xs flex items-center font-bold leading-sm px-1 py-1 rounded-md"
-            style={{
-              background: "var(--theme-color)",
-              color: "rgba(var(--tw-colors-i-white), var(--tw-text-opacity))",
-            }}
+            className="text-xs flex items-center font-semibold p-1 rounded-md bg-accent text-white"
             key={tag}
           >
             <span>{tag}</span>
@@ -76,7 +69,7 @@ export function TagInput({ userTags = [], className, id, name }: Props) {
       </div>
       <Combobox value={selected} onChange={onComboboxChange}>
         <div className="relative mt-1">
-          <div className="relative flex w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+          <div className="relative flex w-full cursor-default overflow-hidden rounded-lg bg-white text-left sm:text-sm">
             <Combobox.Input
               onChange={(event) => setQuery(event.target.value)}
               autoComplete="off"
@@ -86,7 +79,7 @@ export function TagInput({ userTags = [], className, id, name }: Props) {
             />
             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
-                className="h-5 w-5 text-gray-400"
+                className="h-5 w-5 text-black"
                 aria-hidden="true"
               />
             </Combobox.Button>
@@ -98,45 +91,23 @@ export function TagInput({ userTags = [], className, id, name }: Props) {
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {filteredTags.length === 0 && query !== "" ? (
-                <Combobox.Option value={query}>
-                  Create new tag: {query}
+            <Combobox.Options className="absolute max-h-60 w-full overflow-auto rounded-md bg-white p-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 sm:text-sm">
+              {filteredTags.map((tag) => (
+                <Combobox.Option
+                  key={tag}
+                  className={({ active }) =>
+                    cn(
+                      "relative cursor-default select-none py-2 px-4",
+                      active ? "bg-accent text-white" : "text-black",
+                    )
+                  }
+                  value={tag}
+                >
+                  <span className={cn("block truncate font-normal")}>
+                    {tag}
+                  </span>
                 </Combobox.Option>
-              ) : (
-                filteredTags.map((tag) => (
-                  <Combobox.Option
-                    key={tag}
-                    className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? "bg-yellow-700 text-white" : "text-gray-900"
-                      }`
-                    }
-                    value={tag}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={`block truncate ${
-                            selected ? "font-medium" : "font-normal"
-                          }`}
-                        >
-                          {tag}
-                        </span>
-                        {selected ? (
-                          <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? "text-white" : "text-teal-600"
-                            }`}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Combobox.Option>
-                ))
-              )}
+              ))}
             </Combobox.Options>
           </Transition>
         </div>
