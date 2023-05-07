4dd4befd20e4c08c8c233efe92ed7e0d2c230968
Innei <tukon479@gmail.com>
2023-05-07 20:00:50 +0800

    fix: remove react-scroll
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/app/api/healthcheck/route.ts
+++ src/app/api/healthcheck/route.ts
@@ -1,5 +1,6 @@
 import { NextServerResponse } from "~/lib/server-helper"
 
+// /api/healthcheck
 export async function GET() {
   const res = new NextServerResponse()
   return res.status(200).json({
--- src/markdown/index.ts
+++ src/markdown/index.ts
@@ -3,7 +3,6 @@ import type { Root } from "mdast"
 import { Result as TocResult, toc } from "mdast-util-toc"
 import { ReactElement, createElement } from "react"
 import { toast } from "react-hot-toast"
-import { Element } from "react-scroll"
 import { refractor } from "refractor"
 import jsx from "refractor/lang/jsx"
 import tsx from "refractor/lang/tsx"
@@ -217,7 +216,6 @@ export const renderPageContent = (
         createElement: createElement,
         components: {
           img: ZoomedImage,
-          anchor: Element,
           mention: Mention,
           mermaid: Mermaid,
           audio: APlayer,
