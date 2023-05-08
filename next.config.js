8d32961f0e63398521c907d94cc9f7262390d946
runjuu <hi@runjuu.com>
2023-05-08 15:16:58 -0700

    fix: `connect-kit` related app router issues (#511)

--- next.config.js
+++ next.config.js
@@ -69,6 +69,18 @@ module.exports = withBundleAnalyzer(
 
     webpack(config) {
       config.resolve.fallback = { fs: false } // polyfill node-id3
+
+      // https://github.com/WalletConnect/walletconnect-monorepo/blob/7716e164281c2f531145d682c3658f761fa0a823/providers/universal-provider/src/utils/deepLinks.ts#L39
+      // @walletconnect/universal-provider imports react-native conditionally.
+      // Since this is a NextJS app, simply mark it as external to avoid the webpack bundling warning.
+      config.externals.push("react-native")
+
+      // https://github.com/WalletConnect/walletconnect-utils/blob/b7d7dc003c25dd33ef74c2fac483140f71a51d86/jsonrpc/http-connection/src/http.ts#L2
+      // `@walletconnect/jsonrpc-http-connection` imports `cross-fetch` to support fetch in Node.js. It's unnecessary for Next.JS app.
+      config.resolve.alias["cross-fetch"] = require.resolve(
+        "next/dist/build/polyfills/fetch/index.js",
+      )
+
       return config
     },
 
