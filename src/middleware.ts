d7958c5e8b9f9399dab27f59e680a27178e93356
DIYgod <i@diygod.me>
2023-05-11 10:49:38 +0100

    feat: temporarily disable http redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,12 +23,12 @@ export default async function middleware(req: NextRequest) {
       }
     } catch (error) {}
     if (!cfHttps) {
-      return NextResponse.redirect(
-        `https://${req.headers.get("host")}${req.nextUrl.pathname}${
-          req.nextUrl.search
-        }`,
-        301,
-      )
+      // return NextResponse.redirect(
+      //   `https://${req.headers.get("host")}${req.nextUrl.pathname}${
+      //     req.nextUrl.search
+      //   }`,
+      //   301,
+      // )
     }
   }
 
