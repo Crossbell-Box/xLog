2ae631ea9c49e7252ef19299a75461d012132b33
DIYgod <i@diygod.me>
2023-05-11 23:16:15 +0100

    fix: try to fix hydrate bug on site page

--- src/app/site/[site]/not-found.tsx
+++ src/app/site/[site]/not-found.tsx
@@ -1,4 +1,4 @@
-import { SitePage } from "~/components/site/SitePage"
+import { PageContent } from "~/components/common/PageContent"
 import { SITE_URL } from "~/lib/env"
 import { useTranslation } from "~/lib/i18n"
 
@@ -6,21 +6,23 @@ export default async function NotFound() {
   const { t } = await useTranslation("site")
 
   return (
-    <SitePage
-      page={
-        {
-          metadata: {
-            content: {
-              title: t("404 - Whoops, this page is gone."),
-              content: `
+    <>
+      <article>
+        <div>
+          <h2 className="xlog-post-title text-xl font-bold page-title">
+            {t("404 - Whoops, this page is gone.")}
+          </h2>
+        </div>
+        <PageContent
+          className="mt-10"
+          content={`
 - [Back to Home](/)
 - [All posts](/archives)
 
-![image](${SITE_URL}/assets/404.svg)`,
-            },
-          },
-        } as any
-      }
-    />
+![image](${SITE_URL}/assets/404.svg)`}
+          toc={true}
+        ></PageContent>
+      </article>
+    </>
   )
 }
