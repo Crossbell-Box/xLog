f014dedf76cb7ce71e5c6673d3890f9398f24426
Caspian <zhaodonghao586@outlook.com>
2023-05-13 22:34:21 +0800

    fix: remove querystring dep.

--- src/lib/search-parser.ts
+++ src/lib/search-parser.ts
@@ -1,14 +1,11 @@
 import { headers } from "next/headers"
-import querystring from "querystring"
 
-export const searchParser = <T extends Object>() => {
+export const searchParser = () => {
   const search = headers().get("x-search")
-  return querystring.parse(search?.substring(1) || "") as unknown as
-    | Partial<T>
-    | undefined
+
+  return new URLSearchParams(search?.substring(1) || "")
 }
 
 export const isOnlyContent = () => {
-  const parsed = searchParser<{ "only-content": boolean }>()
-  return parsed?.["only-content"]
+  return searchParser().get("only-content")
 }
