d3ced8be45f5d566ab0d2e0b9ec7fce1bfd79dff
DIYgod <i@diygod.me>
2023-05-16 01:34:09 +0100

    feat: exclude pre-caching of some low-frequency files

--- next.config.js
+++ next.config.js
@@ -7,9 +7,14 @@ const execSync = require("child_process").execSync
 
 const cache = require("@ducanh2912/next-pwa").runtimeCaching
 const withPWA = require("@ducanh2912/next-pwa").default({
-  disable: process.env.NODE_ENV === "development",
+  // disable: process.env.NODE_ENV === "development",
   dest: "public",
   publicExcludes: ["*"],
+  buildExcludes: [
+    /\.map$/,
+    /^manifest.*\.js$/,
+    /^\/_next\/static\/chunks\/app\/dashboard/,
+  ],
   workboxOptions: {
     runtimeCaching: [
       {
