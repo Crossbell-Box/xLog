5d9f49219389577b5deb6281b1afc8891136f5fe
Innei <tukon479@gmail.com>
2023-06-06 01:19:45 +0800

    feat: hide post side actions if footer actions showing
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/components/site/PostFooter.tsx
+++ src/components/site/PostFooter.tsx
@@ -1,16 +1,41 @@
+"use client"
+
+import { useInViewport } from "ahooks"
+import { useEffect, useRef } from "react"
+
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+
 import { Comment } from "~/components/common/Comment"
 import { ReactionLike } from "~/components/common/ReactionLike"
 import { ReactionMint } from "~/components/common/ReactionMint"
 import { ReactionTip } from "~/components/common/ReactionTip"
 import { ExpandedCharacter, ExpandedNote } from "~/lib/types"
 
+const key = ["PostFooterInView"]
+
+export const usePostFooterInView = () => {
+  const { data } = useQuery<boolean>({
+    queryKey: key,
+    enabled: false,
+  })
+  return data
+}
 export const PostFooter: React.FC<{
   page?: ExpandedNote
   site?: ExpandedCharacter
 }> = ({ page, site }) => {
+  const actionElRef = useRef<HTMLDivElement>(null)
+  const [isInView] = useInViewport(actionElRef)
+
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    queryClient.setQueryData(key, () => isInView)
+  }, [isInView])
+
   return (
     <>
       <div
+        ref={actionElRef}
         className="xlog-reactions flex fill-gray-400 text-gray-500 sm:items-center space-x-6 sm:space-x-10 mt-14 mb-12"
         data-hide-print
       >
