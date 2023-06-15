edff400905249b08a2c2d2fd2264a47b4bebdcda
DIYgod <i@diygod.me>
2023-06-15 19:55:11 +0100

    feat: remove remark-pangu

--- src/markdown/index.ts
+++ src/markdown/index.ts
@@ -46,7 +46,6 @@ import { rehypeWrapCode } from "./rehype-wrap-code"
 import { rehypeExternalLink } from "./rehyper-external-link"
 import { remarkCallout } from "./remark-callout"
 import { remarkMermaid } from "./remark-mermaid"
-import { remarkPangu } from "./remark-pangu"
 import { remarkYoutube } from "./remark-youtube"
 import sanitizeScheme from "./sanitize-schema"
 
@@ -124,7 +123,6 @@ export const renderPageContent = (
       .use(remarkMath, {
         singleDollarTextMath: false,
       })
-      .use(remarkPangu)
       .use(() => (tree) => {
         env.toc = toc(tree, { tight: true, ordered: true })
       })
