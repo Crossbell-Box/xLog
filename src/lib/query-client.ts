473ba2e36723c5e94ab87f3440917cc3a3201f73
DIYgod <i@diygod.me>
2023-07-13 11:21:12 +0100

    feat: set react-query cache time

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -2,5 +2,16 @@ import { cache } from "react"
 
 import { QueryClient } from "@tanstack/react-query"
 
-const getQueryClient = cache(() => new QueryClient())
+const getQueryClient = cache(
+  () =>
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 5 * 1000,
+          cacheTime: 60 * 1000,
+        },
+      },
+    }),
+)
+
 export default getQueryClient
