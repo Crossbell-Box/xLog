3be5ef4311009fdb7543a1426d103ed6f38756ce
DIYgod <i@diygod.me>
2023-08-13 00:57:37 +0100

    feat: stable feed count

--- src/models/home.model.ts
+++ src/models/home.model.ts
@@ -785,21 +785,48 @@ export async function getFeed({
     }
   }
 
+  let isFiltered = false
   resultAll.list = resultAll.list
-    .filter(
-      (post) =>
-        countCharacters(post?.metadata?.content?.content || "") >
-          (post?.metadata?.content?.tags?.[0] === "comment"
-            ? 6
-            : post?.metadata?.content?.tags?.[0] === "portfolio"
-            ? -1
-            : 300) &&
-        !(new Date(post.metadata?.content?.date_published || "") > new Date()),
-    )
+    .filter((post) => {
+      let limit = 300
+      switch (post?.metadata?.content?.tags?.[0]) {
+        case "comment":
+          limit = 6
+          break
+        case "portfolio":
+          limit = -1
+          break
+      }
+      const pass =
+        countCharacters(post?.metadata?.content?.content || "") > limit &&
+        !(new Date(post.metadata?.content?.date_published || "") > new Date())
+      if (!pass) {
+        isFiltered = true
+      }
+      return pass
+    })
     .map((post) => {
       delete post.metadata?.content.content
       return post
     })
+
+  if (isFiltered && resultAll.list.length < limit && resultAll.cursor) {
+    const next = await getFeed({
+      type,
+      cursor: resultAll.cursor,
+      limit: limit - resultAll.list.length,
+      characterId,
+      daysInterval,
+      searchKeyword,
+      searchType,
+      tag,
+      useHTML,
+      topic,
+    })
+    resultAll.list = resultAll.list.concat(next.list)
+    resultAll.cursor = next.cursor
+    resultAll.count = next.count
+  }
 
   return resultAll
 }
