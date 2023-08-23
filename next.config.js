a6478baddfe876c3be7991920a94f351d299a7b6
DIYgod <i@diygod.me>
2023-08-23 19:43:36 +0100

    feat: stale-while-revalidate for apis

--- next.config.js
+++ next.config.js
@@ -83,6 +83,15 @@ module.exports = withBundleAnalyzer({
           },
         ],
       },
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate",
+          },
+        ],
+      },
     ]
   },
   staticPageGenerationTimeout: 3600,
