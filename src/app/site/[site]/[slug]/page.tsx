170975fc3dd8c7cb0846f4aa8f257f3aa5a16ad5
DIYgod <i@diygod.me>
2023-09-19 00:31:28 +0800

    fix: page title for shorts

--- src/app/site/[site]/[slug]/page.tsx
+++ src/app/site/[site]/[slug]/page.tsx
@@ -40,15 +40,17 @@ export async function generateMetadata({
     queryClient,
   )
 
-  const title = `${page?.metadata?.content?.title} - ${
-    site?.metadata?.content?.name || site?.handle
-  }`
+  const title = `${
+    page?.metadata?.content?.title || page?.metadata?.content?.content
+  } - ${site?.metadata?.content?.name || site?.handle}`
 
   const description = page?.metadata?.content?.summary
 
   const siteImages =
     site?.metadata?.content?.avatars?.[0] || `${SITE_URL}/assets/logo.svg`
-  const images = toGateway(page?.metadata?.content?.cover || siteImages)
+  const images = page?.metadata?.content?.images
+    ? page?.metadata?.content?.images.map((image) => toGateway(image))
+    : [siteImages]
   const useLargeOGImage = !!page?.metadata?.content?.cover
   const twitterCreator = site?.metadata?.content?.connected_accounts
     ?.find((account) => account?.endsWith?.("@twitter"))
