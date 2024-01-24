6bf0285316a7da9c1c3af0a732cce746d1d2dfce
DIYgod <i@diygod.me>
2024-01-25 01:18:43 +0800

    fix: dayjs plugins

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -5,16 +5,20 @@ import "dayjs/locale/ja"
 import "dayjs/locale/zh"
 import "dayjs/locale/zh-tw"
 
+import advancedFormat from "dayjs/plugin/advancedFormat"
 import duration from "dayjs/plugin/duration"
 import localizedFormat from "dayjs/plugin/localizedFormat"
 import relativeTime from "dayjs/plugin/relativeTime"
 import tz from "dayjs/plugin/timezone"
 import utc from "dayjs/plugin/utc"
+import weekOfYear from "dayjs/plugin/weekOfYear"
 
 dayjs.extend(localizedFormat)
 dayjs.extend(utc)
 dayjs.extend(tz)
 dayjs.extend(duration)
 dayjs.extend(relativeTime)
+dayjs.extend(weekOfYear)
+dayjs.extend(advancedFormat)
 
 export default dayjs
