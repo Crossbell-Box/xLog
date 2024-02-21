dc2a2597b295fb15a4213304d161b51d73747fc8
Nagi-ovo <13264500190@163.com>
2024-02-22 02:21:23 +0800

    feat:resize image shortcut

---
+++ src/components/dashboard/toolbars/ResizeImage.tsx
@@ -0,0 +1,27 @@
+import { type EditorView } from "@codemirror/view"
+
+import { ICommand, wrapExecute } from "."
+
+const action = (view: EditorView) => {
+  wrapExecute({
+    view,
+    prepend: ':::div{style="max=width: 300px"}\n',
+    append: "\n:::",
+  })
+}
+
+export const ResizeImage: ICommand = {
+  name: "image-size",
+  label: "Image Size",
+  icon: "i-mingcute-photo-album-2-fill",
+  execute: ({ view }) => {
+    action(view)
+  },
+  shortcut: {
+    key: "Mod-Shift-s",
+    run(view) {
+      action(view)
+      return true
+    },
+  },
+}
