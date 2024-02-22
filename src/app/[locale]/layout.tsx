6072e0aaf355048f4e21c2ca14f16c1ac8a8263a
DIYgod <i@diygod.me>
2024-02-23 03:28:03 +0800

    feat: use snpro font

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,6 +8,7 @@ import "~/css/main.css"
 import { Viewport } from "next"
 import { NextIntlClientProvider } from "next-intl"
 import { getMessages, unstable_setRequestLocale } from "next-intl/server"
+import localFont from "next/font/local"
 import { headers } from "next/headers"
 import { notFound } from "next/navigation"
 import Script from "next/script"
@@ -100,6 +101,73 @@ export const generateMetadata = withHrefLang(async () => ({
   },
 }))
 
+const snPro = localFont({
+  variable: "--font-fans",
+  preload: false,
+  src: [
+    {
+      path: "../../font/SNPro-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../font/SNPro-ThinItalic.woff2",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../../font/SNPro-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../font/SNPro-LightItalic.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../font/SNPro-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../font/SNPro-RegularItalic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../font/SNPro-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../font/SNPro-MediumItalic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../font/SNPro-Semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../font/SNPro-SemiboldItalic.woff2",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../../font/SNPro-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../font/SNPro-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+})
+
 export default async function RootLayout({
   children,
   modal,
@@ -132,7 +200,11 @@ export default async function RootLayout({
   const messages = await getMessages()
 
   return (
-    <html lang={params.locale} className={colorScheme} suppressHydrationWarning>
+    <html
+      lang={params.locale}
+      className={`${colorScheme} ${snPro.variable}`}
+      suppressHydrationWarning
+    >
       <head>
         <ColorSchemeInjector />
         <ColorSchemeScript />
