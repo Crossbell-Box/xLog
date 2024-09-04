cf305ff03c4dde1c09e84ce89dec37a7fdb23f5e
DIYgod <i@diygod.me>
2024-09-04 18:57:42 +0800

    feat: add follow platform

--- src/components/site/Platform.tsx
+++ src/components/site/Platform.tsx
@@ -192,6 +192,11 @@ export const PlatformsSyncMap: {
     icon: "/assets/social/debank.png",
     url: "https://debank.com/profile/{username}",
   },
+  follow: {
+    name: "Follow",
+    icon: "/assets/social/follow.svg",
+    url: "https://app.follow.is/profile/{username}",
+  },
 }
 
 export const Platform = ({
